34f38fa86df9ed96031e9943b627d630a49b2e5b
tonisound <antoine.bergel@gmail.com>
2018-06-14 11:02:19 +0200

    File .gitignore added

--- IQ_Loading/defineIQ.m
+++
@@ -1,9 +0,0 @@
-function templateIQ=defineIQ(sizeIQ)                                       % informations correspondant à un bloc IQ
-    templateIQ.real=zeros(sizeIQ,'single');                                % images x*y*nbIQ
-    templateIQ.imag=zeros(sizeIQ,'single');                                % images
-    templateIQ.ID=0;                                                       % numéro de l'image IQ (du bloc)
-    templateIQ.time=0;                                                     % date d'acquisition de l'imageIQ(du bloc)
-    templateIQ.key=0;                                                      % utilisé lors du scan pour vérifier qu'on est bien sur la bonne séquence
-    templateIQ.pos=[0 0 0 0 0];                                            % position de la sonde lors de l'acquisition du bloc !!!a aucun moment on ne sauvegarde la position de la sonde dans la structureIQ
-    templateIQ.wasRead=0;                                                  % permet de savoir si la structure a été lu (pour le passage vers doppler)
-end
--- IQ_Loading/definePos.m
+++
@@ -1,7 +0,0 @@
-function templatePos=definePos()                                           % Défini les paramêtres de la sonde
-    templatePos.pos=[0 0 0 0 0];                                           % Permet de connaitre la position actuelle de la sonde x*y*z + rotation  + précision en x.
-%     templatePos.ID=0; pas utilisé
-%     templatePos.time=0; pas utilisé
-%     templatePos.isReady=0; pas utilisé
-%     templatePos.wasRead=0;
-end
