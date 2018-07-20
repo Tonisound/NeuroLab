78e4710d13da423d0b7db36c938492011a97f83a
tonisound <antoine.bergel@gmail.com>
2018-07-20 17:22:37 +0200

    Menu Region Edition finalized (Option New/Remove/Modify)

--- NeuroLab-1.0/edit_patches.m
+++ NeuroLab-1.0/edit_patches.m
@@ -22,7 +22,7 @@ ax.YLabel.String ='';
 axis(ax,'off');
 
 % Region Table
-w_col = 100;
+w_col = 60;
 w_margin = 4;
 table_region = uitable('Units','normalized',...
     'Position',[0 0 1 1],...
@@ -81,9 +81,9 @@ cancelButton = uicontrol('Style','pushbutton',...
     'Parent',f);
 
 %Graphic position
-ax.Position = [.3 .05 .5 .9];
-f.Position = [.1 .1 .4 .4];
-table_region.Position = [.05 .05 .2 .9];
+ax.Position = [.2 .05 .6 .9];
+f.Position = [.1 .3 .55 .5];
+table_region.Position = [.025 .05 .15 .9];
 
 newButton.Position = [.825 .9 .15 .05];
 drawButton.Position = [.825 .85 .15 .05];
@@ -111,64 +111,96 @@ im.CData = main_im;
 
 % Intialization
 im.AlphaData = ones(size(main_im));
-% Searching patches
+
+% Building str_popup
 patches = flipud(findobj(ax,'Tag','Region'));
 str_popup = [];
 for i = 1:length(patches)
+    str_popup = [str_popup;{patches(i).UserData.UserData.Name}];
+end
+
+% Searching patches
+for i = 1:length(patches)
+    %patches(i).FaceAlpha = 0;
     patches(i).Visible = 'on';
     patches(i).EdgeColor = patches(i).FaceColor;
     patches(i).FaceColor ='none';
     patches(i).LineWidth = 1;
-    patches(i).MarkerSize =1;
-    %patches(i).FaceAlpha = 0;
-    str_popup = [str_popup;{patches(i).UserData.UserData.Name}];
+    patches(i).MarkerSize = 1;
+    
     % changing Tag to handle patches
     patches(i).Tag = patches(i).UserData.UserData.Name;
+    
+    % changing patches UserData
+    patches(i).Tag = patches(i).UserData.UserData.Name;
+    name = patches(i).UserData.UserData.Name;
+    mask = patches(i).UserData.UserData.Mask;
+    patches(i).UserData = [];
+    patches(i).UserData.Name = name;
+    patches(i).UserData.Mask = mask;
+
 end
 table_region.Data = cellstr(str_popup);
 table_region.UserData.patches = patches;
 
 end
 
-
 function newButton_callback(~,~,handles)
+
+global IM;
 
 answer = inputdlg('Enter Region Name','Region creation',[1 60]);
 while contains(char(answer),handles.Region_table.Data)
     answer = inputdlg('Enter Region Name','Invalid name (Region already exists)',[1 60]);
 end
 
-if ~isempty(answer)
-    return
+if isempty(answer)
+    return;
 end
 
+% Getting dots
+drawButton_callback(handles.drawButton,[],handles);
 
-%creation
-delete(handles.Region_table.UserData.patches(selection));
-handles.Region_table.UserData.patches(selection)=[];
-handles.Region_table.Data(selection,:)=[];
+% Patch creation
+color = rand(1,3);
+hq = patch('XData',handles.drawButton.UserData.xdata,...
+    'YData',handles.drawButton.UserData.ydata,...
+    'FaceColor','none',...
+    'EdgeColor',color,...
+    'Tag',char(answer),...
+    'FaceAlpha',.5,...
+    'LineWidth',1,...
+    'Visible','on',...
+    'Parent',handles.AxEdit);
+delete(findobj(handles.AxEdit,'Tag','Marker'));
+delete(findobj(handles.AxEdit,'Tag','Line'));
+handles.drawButton.UserData = [];
 
-% actual creation
-hl = line('XData',X(:),...
-    'YData',Y(:),...
-    'Color',color,...
-    'Tag','Trace_Region',...
-    'HitTest','off',...
-    'Visible','off',...
-    'LineWidth',l_width,...
-    'Parent',handles.RightAxes);
+
+% mask creation
+x_mask = size(IM,1);
+y_mask = size(IM,2);
+new_mask = double(poly2mask(hq.XData,hq.YData,x_mask,y_mask));
+
 % Updating UserData
-s.Name = regions(ind_regions(i)).name;
-s.Mask = regions(ind_regions(i)).mask;
-s.Graphic = hq;
-hq.UserData = hl;
-hl.UserData = s;
+s.Name = char(answer);
+s.Mask = new_mask;
+hq.UserData = s;
+
+%update table
+handles.Region_table.UserData.patches = [handles.Region_table.UserData.patches;hq];
+handles.Region_table.Data = [handles.Region_table.Data;answer];
+
 
 end
-
 
 function drawButton_callback(hObj,~,handles)
 % Draw new temporary patch
+
+marker_size = 5;
+line_width = 1;
+marker_type = 'o';
+marker_color = 'w';
 
 delete(findobj(handles.AxEdit,'Tag','Marker'));
 delete(findobj(handles.AxEdit,'Tag','Line'));
@@ -176,16 +208,17 @@ delete(findobj(handles.AxEdit,'Tag','Line'));
 xdata = [];
 ydata = [];
 [x,y,button] = ginput(1);
+x = round(x);
+y = round(y);
 
 while button==1
     % marker
-    line(x,y,'Tag','Marker','Marker','o','MarkerSize',10,...
-        'MarkerFaceColor','none','MarkerEdgeColor','w',...
-        'Parent',handles.AxEdit);
+    line(x,y,'Tag','Marker','Marker',marker_type,'MarkerSize',marker_size,...
+        'MarkerFaceColor','none','MarkerEdgeColor',marker_color,'Parent',handles.AxEdit);
     % line
     if ~isempty(xdata)
         line([x,xdata(end)],[y,ydata(end)],'Tag','Line',...
-            'LineWidth',1,'Color','w','Parent',handles.AxEdit);
+            'LineWidth',line_width,'Color',marker_color,'Parent',handles.AxEdit);
     end
     xdata = [xdata;x];
     ydata = [ydata;y];
@@ -194,7 +227,7 @@ end
 
 if length(xdata)>1
     line([xdata(1),xdata(end)],[ydata(1),ydata(end)],'Tag','Line',...
-        'LineWidth',1,'Color','w','Parent',handles.AxEdit);
+        'LineWidth',line_width,'Color',marker_color,'Parent',handles.AxEdit);
 end
 
 if ~isempty(xdata)
@@ -207,6 +240,8 @@ end
 function applyButton_callback(~,~,handles)
 % Apply changes in the Edit Figure
 
+global IM;
+
 if ~isempty(handles.drawButton.UserData)
     xdata = handles.drawButton.UserData.xdata;
     ydata = handles.drawButton.UserData.ydata;
@@ -217,6 +252,12 @@ if ~isempty(handles.drawButton.UserData)
         p = findobj(handles.EditFigure,'Tag',str);
         p.XData = xdata;
         p.YData = ydata;
+        
+        % mask creation
+        x_mask = size(IM,1);
+        y_mask = size(IM,2);
+        new_mask = double(poly2mask(p.XData,p.YData,x_mask,y_mask));
+        p.UserData.Mask = new_mask;
     else
         errordlg('Select region to update.');
         return;
@@ -254,32 +295,69 @@ function cancelButton_callback(~,~,handles2)
 end
 
 function okButton_callback(~,~,handles,handles2)
-% Apply changes in the Main Figure
+% Apply changes to the Main Figure
+% handles: main figure 
+% handles2: region edition figure 
 
-true_patches = findobj(handles.CenterAxes,'Tag','Region');
-flag_change = false;
+global LAST_IM;
 
-for i =1:length(true_patches)
-    patch_name = true_patches(i).UserData.UserData.Name;
-    new_patch = findobj(handles2.EditFigure,'Tag',patch_name);
-
-    if length(true_patches(i).XData)~=length(new_patch.XData) || sum(true_patches(i).XData-round(new_patch.XData))~=0
-        flag_change = true;
-        % Update true patch
-        true_patches(i).YData = round(new_patch.YData);
-        true_patches(i).XData = round(new_patch.XData);
-        % Update mask
-        [x_mask,y_mask] = size(true_patches(i).UserData.UserData.Mask);
-        new_mask = poly2mask(new_patch.XData,new_patch.YData,x_mask,y_mask);
-        true_patches(i).UserData.UserData.Mask = double(new_mask);
-    end
+% Deleting old patches
+old_patches = findobj(handles.CenterAxes,'Tag','Region');
+for i=1:length(old_patches)
+    delete(old_patches(i).UserData);
+    delete(old_patches(i));
 end
+
+% Recreating new patches
+patches = handles2.Region_table.UserData.patches;
+
+for i =1:length(patches)
+    p = patches(i);
+    
+    % patch creation
+    hq = patch('XData',p.XData,...
+        'YData',p.YData,...
+        'FaceColor',p.EdgeColor,...
+        'EdgeColor','k',...
+        'Tag','Region',...
+        'FaceAlpha',.5,...
+        'LineWidth',.5,...
+        'ButtonDownFcn',{@click_RegionFcn,handles},...
+        'Visible','off',...
+        'Parent',handles.CenterAxes);
+        
+    % line creation
+    X = [(1:LAST_IM)';NaN];
+    Y = NaN(size(X));
+    l_width = 1;
+    hl = line('XData',X(:),...
+        'YData',Y(:),...
+        'Color',p.EdgeColor,...
+        'Tag','Trace_Region',...
+        'HitTest','off',...
+        'Visible','off',...
+        'LineWidth',l_width,...
+        'Parent',handles.RightAxes);
+    
+    if handles.RightPanelPopup.Value ==3
+        %set([hq;hl],'Visible','on');
+        set(hl,'Visible','on');
+    end
+    boxLabel_Callback(handles.LabelBox,[],handles);
+    boxPatch_Callback(handles.PatchBox,[],handles);
+    
+    % Updating UserData
+    s.Name = p.UserData.Name;
+    s.Mask = p.UserData.Mask;
+    s.Graphic = hq;
+    hq.UserData = hl;
+    hl.UserData = s;
+end
+
 
 % Close figure and actualize traces
 close(handles2.EditFigure);
-if flag_change
-    actualize_traces(handles);
-end
+actualize_traces(handles);
 
 end
 
@@ -293,7 +371,7 @@ if src.Value
     for i =1:length(hm)
         color = hm(i).EdgeColor;
         color_mask = cat(3, color(1)*ones(size(im.CData)),color(2)*ones(size(im.CData)),color(3)*ones(size(im.CData)));
-        mask = hm(i).UserData.UserData.Mask;
+        mask = hm(i).UserData.Mask;
         image('CData',color_mask,...
             'Parent',handles.AxEdit,...
             'Tag','Mask',...
