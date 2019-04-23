7385a38d407dcbcfc7bc13b7030a4e0485593811
tonisound <antoine.bergel@gmail.com>
2019-04-23 12:18:07 +0200

    Scripts Figure 2 updated (legend,etc) + Script Figure 4 created

--- Quick_scripts/script_Figure2.m
+++ Quick_scripts/script_Figure2.m
@@ -1,46 +1,37 @@
 % Article RUN
 % Figure 2
 
-% function script_Figure2
-%     %script_Figure2_main('CORONAL','RUN');
-%     %script_Figure2_main('CORONAL','LEFT_RUNS');
-%     %script_Figure2_main('CORONAL','RIGHT_RUNS');
-% %     script_Figure2_main('DIAGONAL','RIGHT_RUNS');
-% %     script_Figure2_main('DIAGONAL','RUN');
-% %     script_Figure2_main('DIAGONAL','LEFT_RUNS');
-%     script_Figure2_main('ALL','RUN');
-%     script_Figure2_main('ALL','LEFT_RUNS');
-%     script_Figure2_main('ALL','RIGHT_RUNS');
-% end
+function script_Figure2(cur_list,gather_regions)
 
-function script_Figure2(cur_list,timegroup,gather_regions)
-
-if nargin <3
+if nargin <2
     gather_regions = false;
 end
 
-[D,R,S,list_regions] = compute_script_Figure2(cur_list,timegroup);
-plot1_Figure2(R,list_regions,cur_list,timegroup,gather_regions);
-plot2_Figure2(S,list_regions,cur_list,timegroup,gather_regions);
-plot3_Figure2(S,list_regions,cur_list,timegroup,gather_regions);
+timegroup = {'LEFT_RUNS';'RIGHT_RUNS'};
+
+[D,R,S,list_regions] = compute_script_Figure2(cur_list,char(timegroup(1)));
+plot1_Figure2(R,list_regions,cur_list,char(timegroup(1)),gather_regions);
+plot2_Figure2(S,list_regions,cur_list,char(timegroup(1)),gather_regions);
 
 end
 
 function [D,R,S,list_regions] = compute_script_Figure2(cur_list,timegroup)
 
 close all;
-folder = 'I:\NEUROLAB\NLab_Statistics\fUS_PeriEventHistogram';
+global DIR_STATS;
+
+folder = fullfile(DIR_STATS,'fUS_PeriEventHistogram');
 all_files = dir(fullfile(folder,'*_E'));
 index =0;
 
-list_coronal = {'20141216_225758_E';'20141226_154835_E';'20150223_170742_E';'20150224_175307_E';...
-    '20150225_154031_E';'20150226_173600_E';'20150619_132607_E';'20150620_175137_E';...
-    '20150714_191128_E';'20150715_181141_E';'20150716_130039_E';'20150717_133756_E';...
-    '20150724_170457_E';'20150726_152241_E';'20150728_134238_E';'20151126_170516_E';...
-    '20151201_144024_E';'20151202_141449_E';'20151203_113703_E';'20160622_191334_E';...
-    '20160623_123336_E';'20160624_120239_E';'20160628_171324_E';'20160629_134749_E';...
-    '20160629_191304_E'};
-%list_coronal = {'20150223_170742_E';'20150224_175307_E';'20150225_154031_E'};
+% list_coronal = {'20141216_225758_E';'20141226_154835_E';'20150223_170742_E';'20150224_175307_E';...
+%     '20150225_154031_E';'20150226_173600_E';'20150619_132607_E';'20150620_175137_E';...
+%     '20150714_191128_E';'20150715_181141_E';'20150716_130039_E';'20150717_133756_E';...
+%     '20150724_170457_E';'20150726_152241_E';'20150728_134238_E';'20151126_170516_E';...
+%     '20151201_144024_E';'20151202_141449_E';'20151203_113703_E';'20160622_191334_E';...
+%     '20160623_123336_E';'20160624_120239_E';'20160628_171324_E';'20160629_134749_E';...
+%     '20160629_191304_E'};
+list_coronal = {'20150223_170742_E';'20150224_175307_E';'20150225_154031_E'};
 list_diagonal = {'20150227_134434_E';'20150304_150247_E';'20150305_190451_E';'20150306_162342_E';...
     '20150718_135026_E';'20150722_121257_E';'20150723_123927_E';'20150724_131647_E';...
     '20150725_130514_E';'20150725_160417_E';'20150727_114851_E';'20151127_120039_E';...
@@ -49,8 +40,8 @@ list_diagonal = {'20150227_134434_E';'20150304_150247_E';'20150305_190451_E';'20
     '20160630_114317_E';'20160701_130444_E'};
 
 % list of references to search (in order)
-%list_ref = {'SPEED';'ACCEL'};
-%list_ref = {'heta'};
+% list_ref = {'SPEED';'ACCEL'};
+% list_ref = {'heta'};
 % timegroup = 'RIGHT_RUNS';
 % cur_list = 'DIAGONAL';
 
@@ -203,8 +194,20 @@ f_colors = f.Colormap(round(1:64/length(R):64),:);
 all_axes = [];
 margin_w=.01;
 margin_h=.02;
-n_columns = 4;
+if gather_regions
+    n_columns = 4;
+else
+    n_columns = 8;
+end
 n_rows = ceil(length(list_regions)/n_columns);
+val1 = .9;
+val2 = 1;
+tick_width =.5;
+thresh_average = .5;
+all_markers = {'none';'none';'none'};
+all_linestyles = {'--';'-.';'_'};
+patch_alpha = .1;
+        
 % Creating axes
 for ii = 1:n_rows
     for jj = 1:n_columns
@@ -251,16 +254,17 @@ labels_gathered=regexprep(labels_gathered,'.mat','');
 
 % Plotting
 for index = 1:length(R)
+    
     ax = all_axes(index);
     if contains(R(index).region,'-L')
-        marker = 'none';
-        linestyle = '-';%'--';
+        marker = char(all_markers(1));
+        linestyle = char(all_linestyles(1));
     elseif contains(R(index).region,'-R')
-        marker = 'none';
-        linestyle = '-';%'-.';
+        marker = char(all_markers(2));
+        linestyle = char(all_linestyles(2));
     else
-        marker = 'none';
-        linestyle = '-';
+        marker = char(all_markers(3));
+        linestyle = char(all_linestyles(3));
     end
     
     % Main line
@@ -271,9 +275,9 @@ for index = 1:length(R)
         ind_start = R(index).ind_start(j,:);
         ind_end = R(index).ind_end(j,:);
         
-        line('XData',ref_time,'YData',m,...
+        line('XData',ref_time,'YData',m,'Tag',char(list_regions(index)),...
             'Color',f_colors(index,:),'LineWidth',1,'Linestyle',linestyle,...
-            'Marker',marker','MarkerSize',1,'MarkerFaceColor','none',...
+            'Marker',marker','MarkerSize',1,'MarkerFaceColor',f_colors(index,:),...
             'MarkerEdgeColor',f_colors(index,:),'Parent',ax)
         title(ax,labels_gathered(index));
         grid(ax,'on');
@@ -282,182 +286,47 @@ for index = 1:length(R)
         p_xdat = [ref_time,fliplr(ref_time)];
         p_ydat = [m-s,fliplr(m+s)];
         patch('XData',p_xdat(~isnan(p_ydat)),'YData',p_ydat(~isnan(p_ydat)),...
-            'FaceColor',f_colors(index,:),'FaceAlpha',.1,'EdgeColor','none',...
+            'FaceColor',f_colors(index,:),'FaceAlpha',patch_alpha,'EdgeColor','none',...
             'LineWidth',.25,'Parent',ax);
         % ticks on graph
-        val1 = .95;
-        val2 = 1;
+        ax.YLim = [-5;20];
         line('XData',[ref_time(R(index).ind_start(j)),ref_time(R(index).ind_start(j))],...
             'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
-            'LineWidth',.1,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
+            'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
         line('XData',[ref_time(floor(R(index).ind_end(j))),ref_time(floor(R(index).ind_end(j)))],...
             'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
-            'LineWidth',.1,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
+            'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
     end
     
     % axes limits
-    ax.YLim = [min(R(index).m(:),[],'omitnan') max(R(index).m(:),[],'omitnan')];
-    ind_keep = find(sum(~isnan(R(index).m))/size(R(index).m,1)>.5);
+    %ax.YLim = [min(R(index).m(:),[],'omitnan') max(R(index).m(:),[],'omitnan')];
+    ind_keep = find(sum(~isnan(R(index).m),1)/size(R(index).m,1)>thresh_average);
     ax.XLim = [ref_time(ind_keep(1)), ref_time(ind_keep(end))];
-    ax.YLim = [-5;20];
-     
+    %ax.YLim = [-5;20];
+
+end
+
+% Legend once all has been drawn
+all_axes_unique = unique(all_axes);
+for i = 1:length(all_axes_unique)
+    ax = all_axes_unique(i);
+    l = findobj(ax,'Type','line','-not','Tag','Ticks','-not','Tag','');
+    legend(ax,l,regexprep({l(:).Tag}','.mat',''));
 end
 
 f.Units = 'pixels';
 f.Position = [195          59        1045         919];
-saveas(f,fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
-fprintf('Figure Saved [%s].\n',fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
+
+global DIR_STATS;
+folder = fullfile(DIR_STATS,'fUS_PeriEventHistogram');
+fullname = fullfile(folder,sprintf('%s%s',f.Name,'.pdf'));
+saveas(f,fullname);
+%saveas(f,fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
+fprintf('Figure Saved [%s].\n',fullname);
 
 end
 
 function plot2_Figure2(S,list_regions,cur_list,timegroup,gather_regions)
-
-% Drawing results
-f = figure;
-%f.Name = sprintf('Synthesis Hemodynamics Response all_trials [%s | %s]',cur_list,timegroup);
-f.Name = sprintf('Fig2_SynthesisB_%s-%s',cur_list,timegroup);
-colormap(f,'parula');
-f_colors = f.Colormap(round(1:64/length(S):64),:);
-
-% Sixth tab
-all_axes = [];
-margin_w=.02;
-margin_h=.02;
-n_columns = 8;
-n_rows = ceil(length(list_regions)/n_columns);
-% Creating axes
-for ii = 1:n_rows
-    for jj = 1:n_columns
-        index = (ii-1)*n_columns+jj;
-        if index>length(list_regions)
-            continue;
-        end
-        x = mod(index-1,n_columns)/n_columns;
-        y = (n_rows-1-(floor((index-1)/n_columns)))/n_rows;
-        ax = axes('Parent',f);
-        ax.Position= [x+margin_w y+margin_h (1/n_columns)-2*margin_w (1/n_rows)-3*margin_h];
-        ax.XAxisLocation ='origin';
-        ax.Title.String = sprintf('Ax-%02d',index);
-        ax.Title.Visible = 'on';
-        all_axes = [all_axes;ax];
-    end
-end
-
-%gather_regions = false;
-if gather_regions
-    labels_gathered = strrep(list_regions,'-L','');
-    labels_gathered = strrep(labels_gathered,'-R','');
-    [C, ~, ic] = unique(labels_gathered,'stable');
-    % Reposition axes
-    delete(all_axes(length(C)+1:end));
-    n_rows = ceil(length(C)/n_columns);
-    for ii = 1:n_rows
-        for jj = 1:n_columns
-            index = (ii-1)*n_columns+jj;
-            if index>length(C)
-                continue;
-            end
-            x = mod(index-1,n_columns)/n_columns;
-            y = (n_rows-1-(floor((index-1)/n_columns)))/n_rows;
-            ax = all_axes(index);
-            ax.Position= [x+margin_w y+margin_h (1/n_columns)-2*margin_w (1/n_rows)-3*margin_h];       
-        end
-    end
-    all_axes = all_axes(ic);
-else
-    labels_gathered = list_regions;
-end
-labels_gathered=regexprep(labels_gathered,'.mat','');
-
-% Plotting
-for index = 1:length(S)
-    
-    if isempty(S(index).region)
-        continue
-    end
-    
-    ax = all_axes(index);
-    if contains(S(index).region,'-L')
-        marker = 'none';
-        linestyle = '-';%'--';
-    elseif contains(S(index).region,'-R')
-        marker = 'none';
-        linestyle = '-';%'-.';
-    else
-        marker = 'none';
-        linestyle = '-';
-    end
-    
-    % Main line
-    ref_time = mean(S(index).Xdata,1,'omitnan');
-    Ydata = S(index).Ydata;
-    ind_start = S(index).ind_start;
-    ind_end = S(index).ind_end;
-    imagesc(Ydata,'Parent',ax);
-    title(ax,labels_gathered(index));
-    %grid(ax,'on');
-    
-    % ticks on graph
-    line('XData',ind_start,'YData',1:length(ind_start),...
-        'Color','w','LineWidth',.1,'Linestyle','none',...
-        'Marker','.','MarkerSize',1,'MarkerFaceColor','w',...
-        'MarkerEdgeColor','w','Parent',ax);
-    line('XData',ind_end,'YData',1:length(ind_end),...
-        'Color','w','LineWidth',.1,'Linestyle','none',...
-        'Marker','.','MarkerSize',1,'MarkerFaceColor','w',...
-        'MarkerEdgeColor','w','Parent',ax);
-    
-    % recording line
-    ind_new = find(~strcmp(S(index).rat_id,'')==1);
-    for j=1:length(ind_new)
-        line('XData',[ax.XLim(1)-1000 ax.XLim(2)+1000],'YData',[ind_new(j) ind_new(j)]-.5,...
-        'Color','w','LineWidth',.2,'Linestyle','-',...
-        'Marker','none','MarkerSize',1,'MarkerFaceColor','w',...
-        'MarkerEdgeColor','w','Parent',ax);
-    end
-    
-    % potentiation line
-    line('XData',ind_start/2+20*mean(Ydata,2,'omitnan'),'YData',1:length(ind_start),...
-        'Color','r','LineWidth',.1,'Linestyle','-',...
-        'Marker','none','MarkerSize',1,'MarkerFaceColor','r',...
-        'MarkerEdgeColor','w','Parent',ax);
-    
-    % axes limits
-    ax.FontSize = 8;
-    ax.YTick = 1:length(S(index).rat_id);
-    ax.YTickLabel = S(index).rat_id;
-    %labels_rats
-    labels_rats = S(index).rat_id;
-    unique_rats = unique(S(index).rat_id);
-    unique_rats = unique_rats(2:end);
-    for j=1:length(unique_rats)
-        ind_rep = find(strcmp(labels_rats,unique_rats(j))==1);
-        for jj=2:length(ind_rep)
-            labels_rats(ind_rep(jj))={''};
-        end
-    end
-    ax.YTickLabel = labels_rats;
-    
-    ind_keep = find(sum(~isnan(S(index).Xdata))/size(S(index).Xdata,1)>.5);
-    ax.XTick = ind_keep(1):500:ind_keep(end);
-    str_label = [];
-    for i =1:length(ax.XTick)
-        str_label = [str_label;{ref_time(ax.XTick(i))}];
-    end
-    ax.XTickLabel = str_label;
-    ax.XLim = [ind_keep(1),ind_keep(end)];
-    ax.CLim = [-5;20];
-     
-end
-
-f.Units = 'pixels';
-f.Position = [195          59        1045         919];
-saveas(f,fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
-fprintf('Figure Saved [%s].\n',fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
-
-end
-
-function plot3_Figure2(S,list_regions,cur_list,timegroup,gather_regions)
 
 % Drawing results
 f = figure;
@@ -470,8 +339,22 @@ f_colors = f.Colormap(round(1:64/length(S):64),:);
 all_axes = [];
 margin_w=.02;
 margin_h=.02;
-n_columns = 8;
+if gather_regions
+    n_columns = 4;
+else
+    n_columns = 8;
+end
 n_rows = ceil(length(list_regions)/n_columns);
+val1 = .9;
+val2 = 1;
+tick_width = .5;
+thresh_average = .5;
+all_markers = {'none';'none';'none'};
+all_linestyles = {'--';'-.';'_'};
+% all_markers = {'x';'o';'none'};
+% all_linestyles = {'none';'none';'_'};
+patch_alpha = .1;
+
 % Creating axes
 for ii = 1:n_rows
     for jj = 1:n_columns
@@ -520,43 +403,41 @@ labels_gathered=regexprep(labels_gathered,'.mat','');
 for index = 1:length(S)
     
     if isempty(S(index).region)
-        continue
+        continue;
     end
     
     ax = all_axes(index);
     if contains(S(index).region,'-L')
-        marker = 'none';
-        linestyle = '-';%'--';
+        marker = char(all_markers(1));
+        linestyle = char(all_linestyles(1));
     elseif contains(S(index).region,'-R')
-        marker = 'none';
-        linestyle = '-';%'-.';
+        marker = char(all_markers(2));
+        linestyle = char(all_linestyles(2));
     else
-        marker = 'none';
-        linestyle = '-';
+        marker = char(all_markers(3));
+        linestyle = char(all_linestyles(3));
     end
     
-    % all trials
-    for j=1:size(S(index).Xdata,1)
-        ref_time = S(index).Xdata(j,:);
-        ydata = S(index).Ydata(j,:);
-        ind_start = S(index).ind_start(j);
-        ind_end = S(index).ind_end(j);
-    
+%     % all trials
+%     for j=1:size(S(index).Xdata,1)
+%         ref_time = S(index).Xdata(j,:);
+%         ydata = S(index).Ydata(j,:);
+%         ind_start = S(index).ind_start(j);
+%         ind_end = S(index).ind_end(j);
+%     
 %         %trial
 %         line('XData',ref_time,'YData',ydata,...
 %             'Color',f_colors(index,:),'LineWidth',.1,'Linestyle',linestyle,...
 %             'Marker','none','MarkerSize',1,'MarkerFaceColor','none',...
 %             'MarkerEdgeColor',f_colors(index,:),'Parent',ax)
-        % ticks on graph
-        val1 = .95;
-        val2 = 1;
-        line('XData',[ref_time(ind_start),ref_time(ind_start)],...
-            'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
-            'LineWidth',.1,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
-        line('XData',[ref_time(ind_end),ref_time(ind_end)],...
-            'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
-            'LineWidth',.1,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);        
-    end
+%         % ticks on graph
+%         line('XData',[ref_time(ind_start),ref_time(ind_start)],...
+%             'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
+%             'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
+%         line('XData',[ref_time(ind_end),ref_time(ind_end)],...
+%             'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
+%             'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);        
+%     end
     
     %average
     ref_time = mean(S(index).Xdata,'omitnan');
@@ -566,7 +447,7 @@ for index = 1:length(S)
     modifier(modifier==0)=1;
     sem = s./modifier;
     
-    line('XData',ref_time,'YData',m,...
+    line('XData',ref_time,'YData',m,'Tag',char(list_regions(index)),...
         'Color',f_colors(index,:),'LineWidth',1,'Linestyle',linestyle,...
         'Marker',marker','MarkerSize',3,'MarkerFaceColor','none',...
         'MarkerEdgeColor',f_colors(index,:),'Parent',ax)
@@ -578,26 +459,47 @@ for index = 1:length(S)
     p_ydat = [m-sem,fliplr(m+sem)];
     %p_ydat = [m-s,fliplr(m+s)];
     patch('XData',p_xdat(~isnan(p_ydat)),'YData',p_ydat(~isnan(p_ydat)),...
-        'FaceColor',f_colors(index,:),'FaceAlpha',.5,'EdgeColor','none',...
+        'FaceColor',f_colors(index,:),'FaceAlpha',patch_alpha,'EdgeColor','none',...
         'LineWidth',.25,'Parent',ax);
         
     title(ax,labels_gathered(index));
     %grid(ax,'on');
     
     % axes limits
-    frac = sum(~isnan(S(index).Xdata))/size(S(index).Xdata,1);
-    ind_keep = find(frac>.5);
+    ind_keep = find(sum(~isnan(S(index).Ydata),1)/size(S(index).Ydata,1)> thresh_average);
+    ax.XLim = [ref_time(ind_keep(1))-.5, ref_time(ind_keep(end))];
+    ax.YLim = [-5;20];
     %ax.XTick = ref_time(ind_keep(1):500:ind_keep(end));
     %ax.XTickLabel = {'.5';'1.0';'1.5';'2.0'};
-    ax.XLim = [ref_time(ind_keep(1)),ref_time(ind_keep(end))];
-    ax.YLim = [-5;15];
+    
+    % ticks on graph
+    ind_start_m = round(mean(S(index).ind_start));
+    ind_end_m = round(mean(S(index).ind_end));
+    line('XData',[ref_time(ind_start_m),ref_time(ind_start_m)],...
+        'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
+        'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
+    line('XData',[ref_time(ind_end_m),ref_time(ind_end_m)],...
+        'YData',[val1*ax.YLim(2) val2*ax.YLim(2)],...
+        'LineWidth',tick_width,'Tag','Ticks','Color',[.5 .5 .5],'Parent',ax);
      
-     
+end
+
+% Legend once all has been drawn
+all_axes_unique = unique(all_axes);
+for i = 1:length(all_axes_unique)
+    ax = all_axes_unique(i);
+    l = findobj(ax,'Type','line','-not','Tag','Ticks','-not','Tag','');
+    legend(ax,l,regexprep({l(:).Tag}','.mat',''));
 end
 
 f.Units = 'pixels';
 f.Position = [195          59        1045         919];
-saveas(f,fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
-fprintf('Figure Saved [%s].\n',fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
+
+global DIR_STATS;
+folder = fullfile(DIR_STATS,'fUS_PeriEventHistogram');
+fullname = fullfile(folder,sprintf('%s%s',f.Name,'.pdf'));
+saveas(f,fullname);
+%saveas(f,fullfile('C:\Users\Antoine\Desktop\PeriEvent',sprintf('%s%s',f.Name,'.pdf')));
+fprintf('Figure Saved [%s].\n',fullname);
 
 end
