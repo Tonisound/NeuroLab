e8c891221c2f46d5c5abf66e970f670ed518f676
samlaventure <samuel.laventure@gmail.com>
2023-02-10 18:32:07 +0100

    [Feature Update] Script Browse Correlation upgraded: Birds Eye View Added

--- Scripts/article_ripples/script_browse_correlation.m
+++ Scripts/article_ripples/script_browse_correlation.m
@@ -39,34 +39,52 @@ d = d(arrayfun(@(x) ~strcmp(x.name(1),'.'),d));
 all_references = unique({d(:).name}');
 
 pu1 = uicontrol('Units','normalized','Parent',f,'Style','popup','ToolTipString','recording','String',list_files,'Tag','Popup1');
-pu1.Position = [.05 .95 .85 .04];
+pu1.Position = [.05 .95 .75 .04];
 pu1.UserData.Restriction = [];
 b11 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','*','ToolTipString','Selected/Total','Tag','Button11','Value',0);
-b11.Position = [.91 .95 .05 .04];
+b11.Position = [.81 .95 .05 .04];
 b11.UserData.all_options = all_files;
 b11.UserData.selected = true(size(all_files));
 b11.UserData.popup = pu1;
 b11.String = sprintf('%d/%d',sum(b11.UserData.selected),length(b11.UserData.selected));
+b21 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','Batch','ToolTipString','Show selected options in batch mode','Tag','Button21','Value',0);
+b21.Position = [.87 .95 .05 .04];
+b21.UserData.popup = pu1;
+b31 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','All','ToolTipString','Show all options in batch mode','Tag','Button31','Value',0);
+b31.Position = [.93 .95 .05 .04];
+b31.UserData.popup = pu1;
 
 pu2 = uicontrol('Units','normalized','Parent',f,'Style','popup','ToolTipString','timeframe','Value',1,'String',list_ref,'Tag','Popup2');
-pu2.Position = [.05 .9 .85 .04];
+pu2.Position = [.05 .9 .75 .04];
 pu2.UserData.Restriction = [];
 b12 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','*','ToolTipString','Selected/Total','Tag','Button12','Value',0);
-b12.Position = [.91 .9 .05 .04];
+b12.Position = [.81 .9 .05 .04];
 b12.UserData.all_options = all_timeframes;
 b12.UserData.selected = true(size(all_timeframes));
 b12.UserData.popup = pu2;
 b12.String = sprintf('%d/%d',sum(b12.UserData.selected),length(b12.UserData.selected));
+b22 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','Batch','ToolTipString','Show selected options in batch mode','Tag','Button22','Value',0);
+b22.Position = [.87 .9 .05 .04];
+b22.UserData.popup = pu2;
+b32 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','All','ToolTipString','Show all options in batch mode','Tag','Button32','Value',0);
+b32.Position = [.93 .9 .05 .04];
+b32.UserData.popup = pu2;
 
 pu3 = uicontrol('Units','normalized','Parent',f,'Style','popup','ToolTipString','reference','Value',1,'String',list_ref,'Tag','Popup3');
-pu3.Position = [.05 .85 .85 .04];
+pu3.Position = [.05 .85 .75 .04];
 pu3.UserData.Restriction = [];
 b13 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','*','ToolTipString','Selected/Total','Tag','Button13','Value',0);
-b13.Position = [.91 .85 .05 .04];
+b13.Position = [.81 .85 .05 .04];
 b13.UserData.all_options = all_references;
 b13.UserData.selected = true(size(all_references));
 b13.UserData.popup = pu3;
 b13.String = sprintf('%d/%d',sum(b13.UserData.selected),length(b13.UserData.selected));
+b23 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','Batch','ToolTipString','Show selected options in batch mode','Tag','Button23','Value',0);
+b23.Position = [.87 .85 .05 .04];
+b23.UserData.popup = pu3;
+b33 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','All','ToolTipString','Show all options in batch mode','Tag','Button33','Value',0);
+b33.Position = [.93 .85 .05 .04];
+b33.UserData.popup = pu3;
 
 b1 = uicontrol('Units','normalized','Parent',f,'Style','pushbutton','String','Browse','Tag','Button1');
 b1.Position = [.05 .8 .1 .04];
@@ -97,12 +115,20 @@ pu3.KeyPressFcn = {@pu3_key_pressFcn,handles};
 b1.Callback = {@b1_Callback,handles};
 b2.Callback = {@display_Callback,handles};
 e1.Callback = {@e1_Callback};
-cb1.Callback = {@cb1_Callback,handles};
-cb2.Callback = {@cb2_Callback,handles};
+cb1.Callback = {@cb1_Callback,[handles.Ax1;handles.Ax2]};
+cb2.Callback = {@cb2_Callback,handles.Ax1};
 
 b11.Callback = {@dialog_Callback,handles};
 b12.Callback = {@dialog_Callback,handles};
 b13.Callback = {@dialog_Callback,handles};
+
+b21.Callback = {@batch_Callback,handles};
+b22.Callback = {@batch_Callback,handles};
+b23.Callback = {@batch_Callback,handles};
+
+b31.Callback = {@all_Callback,handles};
+b32.Callback = {@all_Callback,handles};
+b33.Callback = {@all_Callback,handles};
 
 f.Units = 'normalized';
 f.Position = [0.0380    0.3065    0.8662    0.4981];
@@ -173,14 +199,14 @@ end
 function dialog_Callback(hObj,~,handles)
 
 W = 185;
-H = 65;
+H = 60;
 ftsize = 10;
 
 all_options = hObj.UserData.all_options;
 selected = hObj.UserData.selected;
 
 f2 = dialog('Units','characters',...
-    'Position',[30 10 W H],...
+    'Position',[30 20 W H],...
     'HandleVisibility','callback',...
     'IntegerHandle','off',...
     'Renderer','painters',...
@@ -429,9 +455,8 @@ switch evnt.Key
 end
 end
 
-function cb1_Callback(hObj,~,handles)
+function cb1_Callback(hObj,~,all_axes)
 
-all_axes = [handles.Ax1; handles.Ax2];
 all_atlas = findobj(all_axes,'Tag','Atlas');
     
 if hObj.Value
@@ -445,9 +470,9 @@ end
 
 end
 
-function cb2_Callback(hObj,~,handles)
+function cb2_Callback(hObj,~,ax)
 
-im1 = findobj(handles.Ax1,'Type','Image');
+im1 = findobj(ax,'Type','Image');
     
 if hObj.Value
     im1.AlphaData= abs(im1.CData)>.15;
@@ -456,6 +481,25 @@ else
     im1.AlphaData= 1;
 end
 
+end
+
+function cb3_Callback(~,~,hObj,all_axes)
+
+edit_min = hObj.UserData.Edit1;
+edit_max = hObj.UserData.Edit2;
+
+for i = 1:length(all_axes)
+    if hObj.Value
+        all_axes(i).CLimMode = 'auto';
+        edit_min.Enable='off';
+        edit_max.Enable='off';
+    else
+        all_axes(i).CLimMode = 'manual';
+        all_axes(i).CLim = [str2double(edit_min.String) str2double(edit_max.String)];
+        edit_min.Enable='on';
+        edit_max.Enable='on';
+    end
+end
 end
 
 function display_Callback(~,~,handles)
@@ -547,8 +591,278 @@ colorbar(ax3);
 % %     im3.CData = im3.CData .* data_mask.mask;
 % end
 
-cb1_Callback(handles.Checkbox1,[],handles);
-cb2_Callback(handles.Checkbox2,[],handles);
+cb1_Callback(handles.Checkbox1,[],[handles.Ax1;handles.Ax2]);
+cb2_Callback(handles.Checkbox2,[],handles.Ax1);
 drawnow;
+
+end
+
+function batch_Callback(hObj,~,handles)
+
+f =  handles.MainFigure;
+ax1 = handles.Ax1;
+ax2 = handles.Ax2;
+ax3 = handles.Ax3;
+
+str_save = f.UserData.str_save;
+dir_save = fullfile(str_save,'NLab_DATA');
+
+color_atlas = 'k';
+linewidth_atlas = .5;
+
+data_dir = f.UserData.data_dir;
+recording = f.UserData.recording;
+timeframe = f.UserData.timeframe;
+reference = f.UserData.reference;
+all_options = hObj.UserData.popup.String;
+
+switch hObj.Tag
+    case 'Button21'
+        flag_popup = 1;
+        str_batch = fullfile('*',timeframe,reference);
+        all_recordings = all_options;
+        all_timeframes = {timeframe};
+        all_references = {reference};
+    case 'Button22'
+        flag_popup = 2;
+        str_batch = fullfile(recording,'*',reference);
+        all_recordings = {recording};
+        all_timeframes = all_options;
+        all_references = {reference};
+
+    case 'Button23'
+        flag_popup = 3;
+        str_batch = fullfile(recording,timeframe,'*');
+        all_recordings = {recording};
+        all_timeframes = {timeframe};
+        all_references = all_options;
+end
+
+f2 = figure('Tag','Batch','Name',str_batch,'Units','normalized');
+clrmenu(f2);
+cb1_status = 'on';
+cb1 = uicontrol('Units','normalized','Parent',f2,'Style','checkbox','String','Atlas on/off','Tag','Checkbox1','Value',1);
+cb1.Position = [.01 0 .1 .05];
+cb3 = uicontrol('Units','normalized','Parent',f2,'Style','checkbox','String','CLimMode auto','Tag','Checkbox3','Value',1);
+cb3.Position = [.11 0 .1 .05];
+e1 = uicontrol('Units','normalized','Parent',f2,'Style','edit','String',-.5,'TooltipString','CLim(1)','Tag','Edit1','Enable','off');
+e1.Position = [.21 .01 .04 .03];
+e2 = uicontrol('Units','normalized','Parent',f2,'Style','edit','String',.5,'TooltipString','CLim(2)','Tag','Edit2','Enable','off');
+e2.Position = [.26 .01 .04 .03];
+cb3.UserData.Edit1=e1;
+cb3.UserData.Edit2=e2;
+
+N = ceil(sqrt(length(all_options)));
+counter = 0;
+all_axes = [];
+
+for i=1:length(all_recordings)
+    cur_recording = char(all_recordings(i));
+    cur_file = strcat(cur_recording,'_nlab');
+
+    if exist(fullfile(dir_save,cur_file,'Atlas.mat'),'file')
+        fprintf('Loading Atlas [File: %s] ... ',cur_file);
+        data_atlas = load(fullfile(dir_save,cur_file,'Atlas.mat'));
+        fprintf('done.\n');
+    end
+            
+    for j=1:length(all_timeframes)
+        cur_timeframe = char(all_timeframes(j));
+            
+        for k=1:length(all_references)
+            cur_reference = char(all_references(k));
+            if exist(fullfile(data_dir,cur_recording,cur_timeframe,cur_reference),'file') 
+                fprintf('Loading Correlation Pattern [%d/%d] [%s] ... ',counter,length(all_options),fullfile(cur_recording,cur_timeframe,cur_reference));
+                data_corr = load(fullfile(data_dir,cur_recording,cur_timeframe,cur_reference,'Correlation_pattern.mat'));
+                fprintf('done.\n');
+            else
+                warning('Could not load [%d/%d] [%s].',counter,length(all_options),fullfile(cur_recording,cur_timeframe,cur_reference));
+                continue;
+            end
+
+            counter = counter+1;
+            cur_option = char(all_options(counter));
+            
+            ax = subplot(N,N,counter,'Parent',f2);
+            imagesc(data_corr.Rmax_map,'Parent',ax);
+            hold(ax,'on');
+            plot(data_atlas.line_x,data_atlas.line_z,'Linewidth',linewidth_atlas,'Color',color_atlas,'Parent',ax,'Tag','Atlas','Visible',cb1_status);
+            set(ax,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[]);
+            ax.FontSize = 8;
+            ax.Title.String = strrep(cur_option,'_','-');
+            all_axes = [all_axes;ax];
+        end
+    end
+end
+
+% Addding colorbar to last axis
+colorbar(ax,'southoutside');
+
+% Reorganizing axes 
+N = ceil(sqrt(counter));
+eps1 =.025;
+for i=1:counter
+    ax = all_axes(i);
+    ax.Position = [mod(i-1,N)/N+eps1 1-ceil(i/N)/N+eps1 1/N-1.5*eps1 1/N-1.5*eps1];
+end
+f2.Name = sprintf('%s [%d recordings]',str_batch,counter);
+f2.OuterPosition = [0 0 1 1];
+
+cb1.Callback = {@cb1_Callback,all_axes};
+cb3.Callback = {@cb3_Callback,cb3,all_axes};
+e1.Callback = {@cb3_Callback,cb3,all_axes};
+e2.Callback = {@cb3_Callback,cb3,all_axes};
+
+end
+
+function all_Callback(hObj,~,handles)
+
+f =  handles.MainFigure;
+str_save = f.UserData.str_save;
+dir_save = fullfile(str_save,'NLab_DATA');
+data_dir = f.UserData.data_dir;
+recording = f.UserData.recording;
+timeframe = f.UserData.timeframe;
+reference = f.UserData.reference;
+
+color_atlas = 'k';
+linewidth_atlas = .5;
+
+% Listing all files
+d = dir(fullfile(data_dir,'*','*','*','Correlation_pattern.mat'));
+all_folders = {d(:).folder}';
+all_recordings = handles.Popup1.String;
+all_timeframes = handles.Popup2.String;
+all_references = handles.Popup3.String;
+% Restricting to selection
+ind_restrict = (contains(all_folders,all_recordings).* contains(all_folders,all_timeframes)).* contains(all_folders,all_references);
+d = d(ind_restrict==1);
+all_folders = {d.folder}';
+
+list_options = hObj.UserData.popup.String;
+
+switch hObj.Tag
+    case 'Button31'
+        str_batch = [];
+        for j = 1:length(all_timeframes)
+            for k = 1:length(all_references)
+                str_batch = [str_batch; fullfile('*',all_timeframes(j),all_references(k))];
+            end
+        end
+        
+    case 'Button32'
+        str_batch = [];
+        for i = 1:length(all_recordings)
+            for k = 1:length(all_references)
+                str_batch = [str_batch; fullfile(all_recordings(i),'*',all_references(k))];
+            end
+        end
+        
+    case 'Button33'
+        str_batch = [];
+        for i = 1:length(all_recordings)
+            for j = 1:length(all_timeframes)
+                str_batch = [str_batch; fullfile(all_recordings(i),all_timeframes(j),'*')];
+            end
+        end
+end
+
+f2 = figure('Units','normalized','Tag','All','Name',sprintf('Birds Eye View [%s]',data_dir));
+clrmenu(f2);
+cb1_status = 'on';
+cb1 = uicontrol('Units','normalized','Parent',f2,'Style','checkbox','String','Atlas on/off','Tag','Checkbox1','Value',1);
+cb1.Position = [0 0 .02 .05];
+
+cb3 = uicontrol('Units','normalized','Parent',f2,'Style','checkbox','String','CLimMode auto','Tag','Checkbox3','Value',1);
+cb3.Position = [.11 0 .1 .05];
+e1 = uicontrol('Units','normalized','Parent',f2,'Style','edit','String',-.5,'TooltipString','CLim(1)','Tag','Edit1','Enable','off');
+e1.Position = [.21 .01 .04 .03];
+e2 = uicontrol('Units','normalized','Parent',f2,'Style','edit','String',.5,'TooltipString','CLim(2)','Tag','Edit2','Enable','off');
+e2.Position = [.26 .01 .04 .03];
+cb3.UserData.Edit1=e1;
+cb3.UserData.Edit2=e2;
+
+% Creating uitabgroup
+mP = uipanel('FontSize',8,'Units','normalized','Position',[0 .05 1 .95],'Parent',f2);
+tabgp = uitabgroup('Units','normalized','Position',[0 0 1 1],'Parent',mP,'Tag','TabGroup');
+all_axes_2 = [];
+
+h = waitbar(0,'Please wait');
+
+for i=1:length(str_batch)
+    
+    cur_batch = char(str_batch(i));
+    cur_options = strrep(cur_batch,'*',list_options);
+        
+    tab = uitab('Parent',tabgp,'Title',sprintf('%02d',i),'Units','normalized','Tag',sprintf('Tab%d',i));
+    tab.TooltipString=cur_batch;
+%     tab.UserData.cur_batch = cur_batch;
+    all_axes = [];
+    
+    d_restricted = d(contains(all_folders,cur_options)==1);
+    N = length(d_restricted);
+    n = ceil(sqrt(N));
+        
+    prop = ((i-1)/length(str_batch));
+    waitbar(prop,h,sprintf('Loading [%s]\n %.1f %% completed',cur_batch,100*prop));
+
+    for j=1:N
+        
+        temp = regexp(d_restricted(j).folder,filesep,'split');
+        cur_recording = char(temp(end-2));
+        cur_timeframe = char(temp(end-1));
+        cur_reference = char(temp(end));
+        
+        switch hObj.Tag
+            case 'Button31'
+                cur_option=cur_recording;
+                tab_title=fullfile(cur_timeframe,cur_reference);
+            case 'Button32'
+                cur_option=cur_timeframe;
+                tab_title=fullfile(cur_recording,cur_reference);
+            case 'Button33'
+                cur_option=cur_reference;
+                tab_title=fullfile(cur_recording,cur_timeframe);
+        end
+    
+        fprintf('Loading Correlation Pattern [%d/%d] [%s] ... ',j,N,d_restricted(j).folder);
+        data_corr = load(fullfile(d_restricted(j).folder,d_restricted(j).name));
+        fprintf('done.\n');
+        
+        cur_file = strcat(cur_recording,'_nlab');
+        if exist(fullfile(dir_save,cur_file,'Atlas.mat'),'file')
+            fprintf('Loading Atlas [File: %s] ... ',cur_file);
+            data_atlas = load(fullfile(dir_save,cur_file,'Atlas.mat'));
+            fprintf('done.\n');
+        end
+        
+        ax = subplot(n,n,j,'Parent',tab);
+        eps =.02;
+        ax.Position = [mod(j-1,n)/n+eps 1-ceil(j/n)/n+eps 1/n-2*eps 1/n-2*eps];
+        imagesc(data_corr.Rmax_map,'Parent',ax);
+        hold(ax,'on');
+        plot(data_atlas.line_x,data_atlas.line_z,'Linewidth',linewidth_atlas,'Color',color_atlas,'Parent',ax,'Tag','Atlas','Visible',cb1_status);
+        set(ax,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[]);
+        ax.FontSize = 8;
+        ax.Title.String = strrep(cur_option,'_','-');
+        all_axes = [all_axes;ax];
+        
+    end
+    tab.Title=tab_title;
+    tab.UserData.all_axes = all_axes;
+    all_axes_2 = [all_axes_2;all_axes];
+    
+    % Addding colorbar to last axis
+    colorbar(ax,'eastoutside');
+end
+
+close(h);
+% f2.Name = sprintf('%s [%d recordings]',str_batch,counter);
+f2.OuterPosition = [0 0 1 1];
+
+cb1.Callback = {@cb1_Callback,all_axes_2};
+cb3.Callback = {@cb3_Callback,cb3,all_axes_2};
+e1.Callback = {@cb3_Callback,cb3,all_axes_2};
+e2.Callback = {@cb3_Callback,cb3,all_axes_2};
 
 end
